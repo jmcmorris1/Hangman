93c20397ae8d5918b86c9490934a63c94b874d82
Jameel McMorris <jmac54@belsouth.net>
2017-06-22 15:12:53 -0400

    hang em

--- logic.js
+++ logic.js
@@ -1,141 +1,223 @@
-// Array of Word Options (all lowercase)
-var wordsList = [
-    "matt",
-    "roberto",
-    "keythe",
-    "laurie",
-    "ashley",
-    "london",
-    "andrew",
-    "monica",
-    "valyn",
-    "cashuna",
-    "trenton",
-    "karlos"
-];
-// Solution will be held here.
-var chosenWord = "";
-// This will break the solution into individual letters to be stored in array.
-var lettersInChosenWord = [];
-// This will be the number of blanks we show based on the solution
-var numBlanks = 0;
-// Holds a mix of blank and solved letters (ex: 'n, _ _, n, _').
-var blanksAndSuccesses = [];
-// Holds all of the wrong guesses
-var wrongGuesses = [];
-// Game counters
-var winCounter = 0;
-var lossCounter = 0;
-var numGuesses = 9;
-// FUNCTIONS (These are bits of code that we will call upon to run when needed)
-// =========================================================================================
-// startGame()
-// Its how we we will start and restart the game.
-// (Note: It's not being run here. It's just being made for future use.)
-function startGame() {
-    // Reset the guesses back to 0.
-    numGuesses = 9;
-    // Solution is chosen randomly from wordList.
-    chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
-    // The word is broken into individual letters.
-    lettersInChosenWord = chosenWord.split("");
-    // We count the number of letters in the word.
-    numBlanks = lettersInChosenWord.length;
-    // We print the solution in console (for testing).
-    console.log(chosenWord);
-    // CRITICAL LINE - Here we *reset* the guess and success array at each round.
-    blanksAndSuccesses = [];
-    // CRITICAL LINE - Here we *reset* the wrong guesses from the previous round.
-    wrongGuesses = [];
-    // Fill up the blanksAndSuccesses list with appropriate number of blanks.
-    // This is based on number of letters in solution.
-    for (var i = 0; i < numBlanks; i++) {
-        blanksAndSuccesses.push("_");
+// Creating a giant hangmanGame object that will house all of our logic and variables.
+var hangmanGame = {
+  // Object of all words that can be chosen, along with info such as their picture and a song clip.
+  wordsToPick: {
+    genesis: {
+      picture: "genesis.jpg",
+      song: "Illegal Alien",
+      preview: "https://p.scdn.co/mp3-preview/b29a2b925b9654e0efaaff37504fd234307e0ad8"
+    },
+    madonna: {
+      picture: "madonna.jpg",
+      song: "Material Girl",
+      preview: "https://p.scdn.co/mp3-preview/5ff7f7b7d2af1a747da275bed3c49054c01b5b1c"
+    },
+    toto: {
+      picture: "toto.jpg",
+      song: "Rosanna",
+      preview: "https://p.scdn.co/mp3-preview/7cef811eaeb7c7b98245750e73d9d68e9008f317"
+    },
+    queen: {
+      picture: "queen.jpg",
+      song: "Princes of the Universe",
+      preview: "https://p.scdn.co/mp3-preview/b84f24300476f3d3f20056d2388cc51db2e3891f"
+    },
+    u2: {
+      picture: "u2.jpg",
+      song: "With or Without You",
+      preview: "https://p.scdn.co/mp3-preview/28365dff1890075c1371628371cd0e5bbff9a3a3"
+    },
+    metallica: {
+      picture: "metallica.jpg",
+      song: "Master of Puppets",
+      preview: "https://p.scdn.co/mp3-preview/60e6f8dab43f176dd9fb5e795d4e6459bad52e9e"
+    },
+    journey: {
+      picture: "journey.jpg",
+      song: "Don't Stop Believin'",
+      preview: "https://p.scdn.co/mp3-preview/21b9abd3cd2eea634e17a917196fdd5ba2e82670"
+    },
+    inxs: {
+      picture: "inxs.jpg",
+      song: "Need You Tonight",
+      preview: "https://p.scdn.co/mp3-preview/61b17a335d5afc1c4086b1b08e2400f0da147977"
+    },
+    poison: {
+      picture: "poison.jpg",
+      song: "Fallen Angel",
+      preview: "https://p.scdn.co/mp3-preview/0365ad1f152f1834b42b857c4625191cebf9f987"
+    },
+    rush: {
+      picture: "rush.jpg",
+      song: "Limelight",
+      preview: "https://p.scdn.co/mp3-preview/154987dfb07f2fc5ed7aa4d76b80c5dc08ff4d6b"
+    },
+    blondie: {
+      picture: "blondie.jpg",
+      song: "Call Me",
+      preview: "https://p.scdn.co/mp3-preview/ed5a443bc86176135ebca8a114f66f4d814d4c90"
     }
-    // Print the initial blanks in console.
-    console.log(blanksAndSuccesses);
-    // Reprints the guessesLeft to 9
-    document.getElementById("guesses-left").innerHTML = numGuesses;
-    // Prints the blanks at the beginning of each round in the HTML
-    document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
-    // Clears the wrong guesses from the previous round
-    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
-}
-// checkLettesr() function
-// It's where we will do all of the comparisons for matches.
-// Again, it's not being called here. It's just being made for future use.
-function checkLetters(letter) {
-    // This boolean will be toggled based on whether or not a user letter is found anywhere in the word.
-    var letterInWord = false;
-    // Check if a letter exists inside the array at all.
-    for (var i = 0; i < numBlanks; i++) {
-        if (chosenWord[i] === letter) {
-            // If the letter exists then toggle this boolean to true. This will be used in the next step.
-            letterInWord = true;
-        }
+  },
+  // Variables that set the initial state of our hangman game.
+  wordInPlay: null,
+  lettersOfTheWord: [],
+  matchedLetters: [],
+  guessedLetters: [],
+  guessesLeft: 0,
+  totalGuesses: 0,
+  letterGuessed: null,
+  wins: 0,
+  // The setupGame method is called when the page first loads.
+  setupGame: function() {
+    // Here we pick a random word.
+    var objKeys = Object.keys(this.wordsToPick);
+    this.wordInPlay = objKeys[Math.floor(Math.random() * objKeys.length)];
+    // Split the chosen word up into its individual letters.
+    this.lettersOfTheWord = this.wordInPlay.split("");
+    // Builds the representation of the word we are trying to guess and displays it on the page.
+    // At the start it will be all underscores since we haven't guessed any letters ("_ _ _ _").
+    this.rebuildWordView();
+    // This function sets the number of guesses the user gets, and renders it to the HTML.
+    this.processUpdateTotalGuesses();
+  },
+  // This function is run whenever the user guesses a letter..
+  updatePage: function(letter) {
+    // If the user has no guesses left, restart the game.
+    if (this.guessesLeft === 0) {
+      this.restartGame();
     }
-    // If the letter exists somewhere in the word, then figure out exactly where (which indices).
-    if (letterInWord) {
-        // Loop through the word.
-        for (i = 0; i < numBlanks; i++) {
-            // Populate the blanksAndSuccesses with every instance of the letter.
-            if (chosenWord[i] === letter) {
-                // Here we set the specific space in blanks and letter equal to the letter when there is a match.
-                blanksAndSuccesses[i] = letter;
-            }
-        }
-        // Logging for testing.
-        console.log(blanksAndSuccesses);
-    }
-    // If the letter doesn't exist at all...
+    // Otherwise...
     else {
-        // ..then we add the letter to the list of wrong letters, and we subtract one of the guesses.
-        wrongGuesses.push(letter);
-        numGuesses--;
+      // Check for and handle incorrect guesses.
+      this.updateGuesses(letter);
+      // Check for and handle correct guesses.
+      this.updateMatchedLetters(letter);
+      // Rebuild the view of the word. Guessed letters are revealed, unguessed letters have a "_".
+      this.rebuildWordView();
+      // If the user wins, restart the game.
+      if (this.updateWins() === true) {
+        this.restartGame();
+      }
     }
-}
-// roundComplete() function
-// Here we will have all of the code that needs to be run after each guess is made
-function roundComplete() {
-    // First, log an initial status update in the console telling us how many wins, losses, and guesses are left.
-    console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
-    // Update the HTML to reflect the new number of guesses. Also update the correct guesses.
-    document.getElementById("guesses-left").innerHTML = numGuesses;
-    // This will print the array of guesses and blanks onto the page.
-    document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
-    // This will print the wrong guesses onto the page.
-    document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
-    // If we have gotten all the letters to match the solution...
-    if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
-        // ..add to the win counter & give the user an alert.
-        winCounter++;
-        alert("You win!");
-        // Update the win counter in the HTML & restart the game.
-        document.getElementById("win-counter").innerHTML = winCounter;
-        startGame();
+  },
+  // This function governs what happens when the user makes an incorrect guess (that they haven't guessed before).
+  updateGuesses: function(letter) {
+    // If the letter is not in the guessedLetters array, and the letter is not in the lettersOfTheWord array..
+    if ((this.guessedLetters.indexOf(letter) === -1) && (this.lettersOfTheWord.indexOf(letter) === -1)) {
+      // Add the letter to the guessedLetters array.
+      this.guessedLetters.push(letter);
+      // Decrease guesses by one.
+      this.guessesLeft--;
+      // Update guesses remaining and guesses letters on the page.
+      document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+      document.querySelector("#guessed-letters").innerHTML =
+      this.guessedLetters.join(", ");
     }
-    // If we've run out of guesses..
-    else if (numGuesses === 0) {
-        // Add to the loss counter.
-        lossCounter++;
-        // Give the user an alert.
-        alert("You lose");
-        // Update the loss counter in the HTML.
-        document.getElementById("loss-counter").innerHTML = lossCounter;
-        // Restart the game.
-        startGame();
+  },
+  // This function sets the initial guesses the user gets.
+  processUpdateTotalGuesses: function() {
+    // The user will get more guesses the longer the word is.
+    this.totalGuesses = this.lettersOfTheWord.length + 5;
+    this.guessesLeft = this.totalGuesses;
+    // Render the guesses left to the page.
+    document.querySelector("#guesses-remaining").innerHTML = this.guessesLeft;
+  },
+  // This function governs what happens if the user makes a successful guess.
+  updateMatchedLetters: function(letter) {
+    // Loop through the letters of the "solution".
+    for (var i = 0; i < this.lettersOfTheWord.length; i++) {
+      // If the guessed letter is in the solution, and we haven't guessed it already..
+      if ((letter === this.lettersOfTheWord[i]) && (this.matchedLetters.indexOf(letter) === -1)) {
+        // Push the newly guessed letter into the matchedLetters array.
+        this.matchedLetters.push(letter);
+      }
     }
-}
-// MAIN PROCESS (THIS IS THE CODE THAT CONTROLS WHAT IS ACTUALLY RUN)
-// ==================================================================================================
-// Starts the Game by running the startGame() function
-startGame();
-// Then initiate the function for capturing key clicks.
+  },
+  // This function builds the display of the word that is currently being guessed.
+  // For example, if we are trying to guess "blondie", it might display "bl_ndi_".
+  rebuildWordView: function() {
+    // We start with an empty string.
+    var wordView = "";
+    // Loop through the letters of the word we are trying to guess..
+    for (var i = 0; i < this.lettersOfTheWord.length; i++) {
+      // If the current letter has been guessed, display that letter.
+      if (this.matchedLetters.indexOf(this.lettersOfTheWord[i]) !== -1) {
+        wordView += this.lettersOfTheWord[i];
+      }
+      // If it hasn't been guessed, display a "_" instead.
+      else {
+        wordView += "&nbsp;_&nbsp;";
+      }
+    }
+    // Update the page with the new string we built.
+    document.querySelector("#current-word").innerHTML = wordView;
+  },
+  // Function that "restarts" the game by resetting all of the variables.
+  restartGame: function() {
+    document.querySelector("#guessed-letters").innerHTML = "";
+    this.wordInPlay = null;
+    this.lettersOfTheWord = [];
+    this.matchedLetters = [];
+    this.guessedLetters = [];
+    this.guessesLeft = 0;
+    this.totalGuesses = 0;
+    this.letterGuessed = null;
+    this.setupGame();
+    this.rebuildWordView();
+  },
+  // Function that checks to see if the user has won.
+  updateWins: function() {
+    // this won't work for words with double or triple letters
+    // var lettersOfTheWordClone = this.lettersOfTheWord.slice(); //clones the array
+    // this.matchedLetters.sort().join('') == lettersOfTheWordClone.sort().join('')
+    // If you haven't correctly guessed a letter in the word yet, we set win to false.
+    var win;
+    if (this.matchedLetters.length === 0) {
+      win = false;
+    }
+    // Otherwise, we set win to true.
+    else {
+      win = true;
+    }
+    // If a letter appears in the lettersOfTheWord array, but not in the matchedLetters array, set win to false.
+    // In English, if you haven't yet guessed all the letters in the word, you don't win yet.
+    for (var i = 0; i < this.lettersOfTheWord.length; i++) {
+      if (this.matchedLetters.indexOf(this.lettersOfTheWord[i]) === -1) {
+        win = false;
+      }
+    }
+    // If win is true...
+    if (win === true) {
+      // Increment wins.
+      this.wins = this.wins + 1;
+      // Update wins on the page.
+      document.querySelector("#wins").innerHTML = this.wins;
+      // Update the song title and band on the page.
+      document.querySelector("#music").innerHTML = this.wordsToPick[this.wordInPlay].song +
+      " By " + this.wordInPlay;
+      // Update the image of the band on the page.
+      document.querySelector("#band-div").innerHTML =
+       "<img class='band-image' src='images/" +
+       this.wordsToPick[this.wordInPlay].picture + "' alt='" +
+       this.wordsToPick[this.wordInPlay].song + "'>";
+      // Play an audio track of the band.
+      var audio = new Audio(this.wordsToPick[this.wordInPlay].preview);
+      audio.play();
+      // return true, which will trigger the restart of our game in the updatePage function.
+      return true;
+    }
+    // If win is false, return false to the updatePage function. The game goes on!
+    else {
+      return false;
+    }
+  }
+};
+// Initialize the game when the page loads.
+hangmanGame.setupGame();
+// When a key is pressed..
 document.onkeyup = function(event) {
-    // Converts all key clicks to lowercase letters.
-    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    // Runs the code to check for correctness.
-    checkLetters(letterGuessed);
-    // Runs the code after each round is done.
-    roundComplete();
+  // Capture pressed key and make it lowercase.
+  hangmanGame.letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+  // Pass the guessed letter into our updatePage function to run the game logic.
+  hangmanGame.updatePage(hangmanGame.letterGuessed);
 };
